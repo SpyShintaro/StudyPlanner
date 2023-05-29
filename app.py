d0203d9f9481bdc94f5fdc0be36c6c08c65f8a4e
SpyShintaro <40752320+SpyShintaro@users.noreply.github.com>
2023-05-29 11:49:38 +1000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,7 @@
+"""
+StudyPlanner/app
+
+Author: Jake Hickey
+Date: 29/05/23
+Description: The GUI that the user will use to interact with the rest of the program
+"""
---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1>Study Planner</h1>
+<em>Developed as part of Software Development Unit 3 & 4</em>
+
+<p>An app to organise your school life.</p>
