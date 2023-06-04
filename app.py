f67d7f76a60e49303a63a647a353cd6cba158997
SpyShintaro <40752320+SpyShintaro@users.noreply.github.com>
2023-06-05 09:53:02 +1000

    First Render of Month on Calendar

--- app.py
+++ app.py
@@ -6,40 +6,75 @@ Date: 29/05/23
 Description: The GUI that the user will use to interact with the rest of the program
 """
 
+# GUI Handling
 import sys
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import *
 
+# Datetime handling
+import calendar
+from datetime import datetime
+from itertools import cycle
+
 class MainWindow(QMainWindow):
     def __init__(self, view):
         super().__init__()
-        self.view = view
 
-        self.initUI()
+        self.initUI(view)
     
-    def initUI(self):
+    def initUI(self, view):
         widget = QWidget()
         main_layout = QGridLayout()
 
         main_layout.addWidget(QGroupBox("Item Information"), 1, 1, 4, 2)
-        main_layout.addWidget(self.view, 1, 3, 4, 7)
+        main_layout.addWidget(view, 1, 3, 4, 7)
 
         widget.setLayout(main_layout)
 
         self.setCentralWidget(widget)
         self.show()
 
-class CalendarView(QWidget):
+class CalendarView(QWidget): # Generates a calendar view containing 35 days
     def __init__(self):
         super().__init__()
 
+        self.initUI()
+    
+    def initUI(self):
         calendar_layout = QGridLayout()
 
-        for week in range(4):
-            for day in range(7):
-                calendar_layout.addWidget(QMessageBox(), week, day)
-        
+        calendar_group = QGroupBox(f"{datetime.today().month}")
+        dates = self.map_dates()[::-1] # A list of dates in the month, spaced with zeroes to start at the correct weekday
+        print(dates)
+
+        for week in range(5): # Iterates through five rows (representing a week)
+            for day in range(7): # Iterates through each day in the week, filling out columns
+                day_box = QMessageBox()
+                
+                try:
+                    date = str(dates.pop())
+
+                    if date != "0":
+                        day_box.setText(date)
+                except IndexError:
+                    pass
+
+                calendar_layout.addWidget(day_box, week, day)
+
         self.setLayout(calendar_layout)
+
+    def map_dates(self):
+        """
+        Maps all dates in the month to weekdays for use in calendar, and returns a tuple containg the first weekday of the month, and the final day
+        """
+        today = datetime.today()
+
+        starting_weekday, month_range = calendar.monthrange(today.year, today.month)
+
+        
+        dates = [0 for day in range(starting_weekday - 1)] + [date for date in range(month_range + 1)] # Adds 0 for each day until the starting weekday of the month, after which it appends the date to each element of the list
+
+        return dates
 
 def main():
     app = QApplication(sys.argv)
