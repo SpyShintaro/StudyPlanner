2eaebe26b327aac4c3a94cf76a90ee02d9857eb8
SpyShintaro <40752320+SpyShintaro@users.noreply.github.com>
2023-07-10 10:52:23 +1000

    Bringing back up-to-date in background logic

--- studytime/core.py
+++ studytime/core.py
@@ -1,3 +1,10 @@
+"""
+studytime.core
+
+Author: Jake Hickey
+Description: A library containing backend logic for StudyTime processes
+"""
+
 # Imports
 if __name__ == "__main__": # Band-Aid fix for if I want to directly test this module in particular (deleting later)
     from time_class import *
@@ -18,18 +25,96 @@ class SaveInstance:
         """
         self.file_name = file_name
         self.data = self.load_file()
+
+    def add_item(self, item_time: datetime, item: object):
+        """
+        A generic method for adding organizational items to the relevant date
+        """
+        year, month, day = item_time.year, item_time.month, item_time.day
+        data = self.get_date(year, month, day)
+
+        data.append(item.prepare_dict())
+        data.sort(key=lambda x : x["time"]) # Sorts the date items by their timestamp in ascending order
+
+        self.save_changes()
     
-    def generate_new_year(self, year) -> list:
+    def remove_item(self, item_name: str, item_time: datetime):
+        """
+        A generic method for removing organizational items to the relevant date
+        """
+
+        year, month, day = item_time.year, item_time.month, item_time.day
+        data = self.get_date(year, month, day)
+
+        for item in data:
+            if item["name"] == item_name and item["time"] == str(item_time.time()): # Should be specific enough to prevent double-ups
+                data.remove(item)
+                self.save_changes()
+                return
+    
+    def add_task(self, task_name: str, task_time: datetime, subject: str):
+        """
+        Takes new task info and appends it to the "data" list for the relevant date, before sorting it
+        """
+        task = Task(task_name, task_time, subject)
+
+        self.add_item(task_time, task)
+    
+    def add_event(self, event_name: str, event_time: datetime):
+        """
+        Takes new event info and appends it to the "data" list for the relevant date, before sorting it
+        """
+        event = Event(event_name, event_time)
+
+        self.add_item(event_time, event)
+    
+    def add_assignment(self, assignment_name: str, assignment_time: datetime, subject: str):
+        """
+        Takes new assignment info and appends it to the "data" list for the relevant date, before sorting it
+        """
+        assignment = Assignment(assignment_name, assignment_time, subject)
+
+        self.add_item(assignment_time, assignment)
+
+    def generate_new_year(self, year: str) -> list:
         """
         Generates a new year instance 
         """
         new_year = []
 
         for month in range(1, 13):
-            current_month = Month(month, year, [Date(date) for date in map_dates(year, month) if date != 0]).prepare_dict()
-            new_year.append({f"{current_month['name']}": current_month['data']}) # Generates a dictionary item for each month, involving the dates
+            current_month = Month(month, [Date(date) for date in map_dates(year, month) if date != 0]).prepare_list()
+            new_year.append(current_month) # Generates a dictionary item for each month, involving the dates
         
         return new_year
+
+    def get_year(self, target_year: str) -> dict:
+        """
+        Returns the dictionary for the target year
+        """
+        for year in self.data:
+            if year["year"] == str(target_year): # Checks if the value of the "year" key is the year being searched for
+                return year
+        
+        return None
+    
+    def get_month(self, target_year: str, target_month: int) -> list:
+        """
+        Returns a list for the target month. Target_month should be passed with the assumption that January = 1
+        """
+        year = self.get_year(target_year)
+
+        return year["months"][target_month - 1]
+
+    def get_date(self, target_year: str, target_month: int, target_date: str) -> list:
+        """
+        Returns a list of all items scheduled for the target date.
+        """
+        month = self.get_month(target_year, target_month)
+
+        for date in month:
+            if date["date"] == str(target_date):
+                return date["data"]
     
     def new_file(self):
         """
@@ -38,31 +123,45 @@ class SaveInstance:
         file_path = f"studytime/app_data/{self.file_name}.json"
         now = datetime.today()
 
-        template = {f"{now.year}": self.generate_new_year(now.year)}
+        template = [{
+            "year": f"{now.year}",
+            "months": self.generate_new_year(now.year)
+        }]
         
         with open(file_path, "w") as f:
-            json.dump(template, f, indent = 4)
+            json.dump(template, f, indent = 4) # Outputs empty template for the current year to a new file
         
         return template
 
-    def update_file(self, year):
+    def add_year(self, year):
         """
-        Gets new year data, and updates the JSON file accordingly
+        Handles adding new years to the file
         """
         file_path = f"studytime/app_data/{self.file_name}.json"
-        template = {f"{year}": self.generate_new_year(year)}
+        template = {
+                    "year": f"{year}",
+                    "months": self.generate_new_year(year)
+                    }
 
         with open(file_path, "r") as f: # Loading JSON file into Python Object
             data = json.load(f)
         
-        data.update(template) # Adding template to the JSON/Python object
+        """        for years in data:
+            if years.keys()[0] == year:
+                break
+            else:
+        """
+
+        data.append(template) # Adding template to the JSON/Python object
 
         with open(file_path, "w") as f: # Rewrites JSON file using updated data
             json.dump(data, f, indent=4)
         
+        print(self.data)
+
         return template
     
-    def load_file(self) -> str:
+    def load_file(self) -> dict:
         """
         Loads json file given the relevant filename. If there's no corresponding file, it makes a new file.
         """
@@ -71,9 +170,16 @@ class SaveInstance:
         try:
             with open(file_path, "r") as f:
                 data = json.load(f)
-                print(data["2023"])
         except FileNotFoundError:
-            print(self.new_file())
+            data = self.new_file()
+
+        return data
+
+    def save_changes(self):
+        file_path = f"studytime/app_data/{self.file_name}.json"
+
+        with open(file_path, "w") as f:
+            json.dump(self.data, f, indent=4)
 
     def organise_times(self, data):
         """
@@ -88,35 +194,81 @@ class Task:
     """
     The standard type of StudyTime object, this indicates any one-time activity that is set for the user to complete by a certain time
     """
-    def __init__(self, name: str, date_set: datetime.date, deadline: datetime, subject):
+    def __init__(self, name: str, date: datetime, subject):
         """
         Creates a dictionary object
         """
         self.name = name
-        self.date_set = date_set
-        self.deadline = deadline
+        self.date = date
+        self.time = date.time()
         self.subject = subject
+        self.completed = False
     
-    def generate_task(self):
+    def prepare_dict(self):
         """
-        Saves the task into various other objects
+        Returns a dictionary for the purpose of storing within the save instance
         """
-        item_data = {
+        data = {
             "name": f"{self.name}",
             "subject": f"{self.subject}",
-            "date set": f"{self.date_set}",
+            "time": f"{self.time}",
+            "completed": False,
+            "type": "Task"
         }
+
+        return data
+
+    def mark_completed(self, complete: bool):
+        self.completed = complete
+
+class Event:
+    """
+    An activity that automatically marks itself as completed after the time slot regardless of user input
+    """
+    def __init__(self, name: str, date: datetime):
+        self.name = name
+        self.date = date
+        self.time = date.time()
+    
+    def prepare_dict(self) -> dict:
+        data = {
+            "name": f"{self.name}",
+            "time": f"{self.time}",
+            "type": "Event"
+        }
+
+        return data
+
+class Assignment:
+    """
+    A cross between an event and an activity, automatically marking itself as completed, while still being tied to a subject
+    """
+    def __init__(self, name: str, date: datetime, subject: str):
+        self.name = name
+        self.date = date
+        self.time = date.time()
+        self.subject = subject
+        self.completed = False
+    
+    def prepare_dict(self):
+        data = {
+            "name": f"{self.name}",
+            "time": f"{self.time}",
+            "subject": f"{self.subject}",
+            "completed": f"{self.completed}",
+            "type": "Assignment"
+        }
+
+        return data
 
 def map_dates(year: int, month: int) -> list[int]:
     """
     Maps all dates in the month to weekdays for use in calendar, and returns a tuple containg the first weekday of the month, and the final day
     """
 
+    year, month = int(year), int(month)
     starting_weekday, month_range = calendar.monthrange(year, month)
     
     dates = [0] * (starting_weekday - 1) + [date for date in range(month_range + 1)] # Adds 0 for each day until the starting weekday of the month, after which it appends the date to each element of the list
 
     return dates
-
-if __name__ == "__main__":
-    save = SaveInstance("date")
--- studytime/time_class.py
+++ studytime/time_class.py
@@ -12,36 +12,38 @@ months = ["January", "February", "March", "April", "May", "June", "July", "Augus
 
 
 class Year:
-    def __init__(self, year_data):
-        self.year = year_data
+    def __init__(self, year_data: dict):
+        self.year = []
+
+        for month, dates in year_data.items():
+            self.year.append(Month(month, dates))
+
 
 class Month:
-    def __init__(self, month: datetime.month, year: datetime.year, date_objects):
-        self.name = f"{months[month - 1]} {year}"
+    def __init__(self, month, date_objects: list):
+        self.month = month
+        self.name = f"{months[int(month) - 1]}"
         self.dates = date_objects
 
-    def prepare_dict(self) -> dict:
+    def prepare_list(self) -> list:
         """
         Maps out data into dictionary for storage in JSON format
         """
-        data = {
-            "name": self.name,
-            "data": [date.prepare_dict() for date in self.dates]
-        }
+        data = [date.prepare_dict() for date in self.dates]
 
         return data
 
 class Date:
     def __init__(self, date: datetime.day) -> None:
         self.name = f"{date}"
-        self.items = {}
+        self.items = []
     
     def add_item(self, item):
         self.items[item["name"]] = {item}
     
     def prepare_dict(self) -> dict:
         data = {
-            "name": self.name,
+            "date": self.name,
             "data": self.items
         }
 
