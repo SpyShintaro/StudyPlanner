f8e1ec4484b63366b43aed87d4b3bdbb77dcb10f
SpyShintaro <jakeislightning@gmail.com>
2023-07-18 14:47:37 +1000

    Implemented basic mockup of new item dialog

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ Description: User-Friendly GUI to interact with StudyTime functionality
 """
 
 # GUI Handling
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLineEdit, QCalendarWidget, QTextEdit, QDialog
+from PyQt6.QtWidgets import (QMainWindow, QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout, QLineEdit, QCalendarWidget, QTextEdit, QDialog,
+                             QDateEdit, QTimeEdit, QComboBox)
+from PyQt6.QtCore import Qt, QDate, QTime
+from PyQt6.QtGui import QFont
 
 import sys
 from studytime.core import *
@@ -30,14 +33,21 @@ class MainWindow(QMainWindow):
         layout = QHBoxLayout() # Parent Layout
 
         info_layout = QVBoxLayout() # Information Box Layout
+
+        self.header_font = QFont("Helvetica", 15)
+        self.header_font.setBold(True)
         
-        self.date_header = QLineEdit(parent=self)
-        self.date_header.setReadOnly(True)
+        self.date_header = QLabel(parent=self)
+        self.date_header.setFont(self.header_font)
 
         self.info_box = QTextEdit(parent=self)
         self.info_box.setReadOnly(True)
 
+        new_item_btn = QPushButton("Create New Item", self)
+        new_item_btn.clicked.connect(self.open_item_dialog)
+
         info_layout.addWidget(self.date_header)
+        info_layout.addWidget(new_item_btn)
         info_layout.addWidget(self.info_box)
 
         self.date = QCalendarWidget(self)
@@ -58,6 +68,13 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("StudyTime")
         self.show()
+
+    def open_item_dialog(self):
+        """
+        Creates and displays a dialog for the user to add a new item
+        """
+        dialog = NewItemDialog(self)
+        dialog.exec()
     
     def data_clicked(self):
         """
@@ -84,9 +101,58 @@ class MainWindow(QMainWindow):
         self.info_box.setText(f"{text}")
 
 class NewItemDialog(QDialog):
-    def __init__(self):
-        pass
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setWindowTitle("Add Item")
+        layout = QGridLayout()
+
+        header = QLabel(self)
+        header.setText("Add New Item")
+        header.setFont(parent.header_font)
         
+        name_text = QLabel("Name: ", self)
+        name_input = QLineEdit(self)
+
+        type_text = QLabel("Type: ", self)
+        
+        type_input = QComboBox(self)
+        type_input.addItems(["Task", "Event", "Assignment"])
+
+        date_text = QLabel("Date: ", self)
+
+        date_input = QDateEdit(self)
+        date_input.setDate(QDate.currentDate())
+        date_input.setCalendarPopup(True)
+
+        time_text = QLabel("Time: ", self)
+
+        time_input = QTimeEdit(self)
+        time_input.setTime(QTime.currentTime())
+
+        # Header
+        layout.addWidget(header, 0, 0)
+
+        # Name Input
+        layout.addWidget(name_text, 1, 0)
+        layout.addWidget(name_input, 1, 1)
+
+        # Type Input
+        layout.addWidget(type_text, 2, 0)
+        layout.addWidget(type_input, 2, 1)
+
+        # Date Input
+        layout.addWidget(date_text, 3, 0)
+        layout.addWidget(date_input, 3, 1)
+
+        # Time Input
+        layout.addWidget(time_text, 4, 0)
+        layout.addWidget(time_input, 4, 1)
+
+        self.setLayout(layout)
+    
+    def add_item(self):
+        pass
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
